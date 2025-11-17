6269cfd3377180e8f584a45775e8c8892793f076
TheJoker793 <channoufi793@gmail.com>
2025-11-17 15:36:17 +0100

    ajout exercice 4

---
+++ src/exercice4/Ex5PanoramaArrays.java
@@ -0,0 +1,48 @@
+package exercice4;
+
+import java.util.Arrays;
+
+public class Ex5PanoramaArrays {
+
+	public static void main(String[] args) {
+		int[]a= {3,1,4,1,5,9};
+		int[]b=Arrays.copyOfRange(a,2,a.length);
+		Arrays.sort(b);
+		int[]c=Arrays.copyOf(b, b.length);
+		if(c.length>2) {
+			for(int i=1;i<c.length-1;i++) {
+				c[i]=42;
+			}
+		}
+		System.out.println("tableau a: "+Arrays.toString(a));
+		System.out.println("tableau b: "+Arrays.toString(b));
+		System.out.println("tableau c: "+Arrays.toString(c));
+	
+		
+		System.out.println("b et c identiques =>" +Arrays.equals(b, c));
+		
+		boolean identique=Arrays.equals(b, c);
+		System.out.println(identique?"ils sont identique":"ne sont pas identiques");
+		int mm=Arrays.mismatch(b, c);
+		if (mm>=0) {
+			System.out.println("le desaccord est à la position "+mm);
+		}
+		else {
+			System.out.println("ideentique ( "+mm+")");
+
+		}
+		
+		String[][] noms = {{"Ben Salah","Mohamed"}, {"Ben Ammar","Ali"},
+				 {"Ben Farhat","Sami"}};
+		System.out.println(Arrays.deepToString(noms));
+		
+		
+	
+	
+		
+
+		
+
+	}
+
+}
